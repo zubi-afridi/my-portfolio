cdfd4d7b4a563f696dbb371389984819ea419011
zubi-afridi <zubairafridi2312@gmail.com>
2026-05-06 11:09:11 +0500

    optimized performance level

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("react")) return "vendor-react";
-            if (id.includes("motion")) return "vendor-motion";
-            if (id.includes("lucide-react") || id.includes("react-icons"))
-              return "vendor-icons";
-            return "vendor";
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-  },
 });
